d81da3b374a102f8aaa4917578976d240c6b0f71
pablovas <pablo71042001@gmail.com>
2024-08-16 17:50:48 -0300

    feat: Improve lazy loading for WRF component images

--- src/services/wrfLocal.js
+++ src/services/wrfLocal.js
@@ -2,11 +2,11 @@ export const wrfLocal = () => {
     const listImage = []; // array de urls
     let hoursSetting = 1; // Valor padrão para hoursSetting
     const storedHourScope = localStorage.getItem("hourScopeWRF");
-  
+
     if (storedHourScope !== null) {
       hoursSetting = parseInt(storedHourScope); // Atualiza hoursSetting com o valor do localStorage
     }
-  
+
     try {
       const currentHour = new Date().getHours();
       let actualDay = new Date().getDate(); //DIA
@@ -17,21 +17,18 @@ export const wrfLocal = () => {
       const dateWRF = actualYear + "" + actualMonth + "" + actualDay;
 
     let h = 0;
-    
+
     if (currentHour) {
-        // Garante que h varia entre 0 e 23
         const paddedHour = h.toString().padStart(2, '0');
         listImage.push(
             `../../public/wrf/${dateWRF - 1}/maxdbz_+_PNM_wrfout_${dateWRF - 1}_fcst_d01_${actualYear}-${actualMonth}-${actualDay}_${paddedHour}_00_00.png`,
         );
-    
+
         h = (h + 1) % 24;
     }
-  
+
     } catch (error) {
       console.error("Erro ao obter os dados do radar:", error);
       throw error;
     }
     return listImage;  };
-
-  
