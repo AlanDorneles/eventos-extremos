54ed30e11f28fbc1d2ceb4c91cdc8385cb6dfa27
pablovas <pablo71042001@gmail.com>
2025-07-24 16:59:41 -0300

    Criando lazy loading

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,27 @@
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import Home from "../pages/Home";
+import { Suspense, lazy } from "react";
 
+const Home = lazy(() => import("../pages/Home"));
 
 export const Root: React.FC = () => {
-  
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/produtos/radar" />} />
-        <Route path="/sobre" element={<Home />} />
-        <Route path="/produtos/radar" element={<Home />} />
-        <Route path="/produtos/satelite" element={<Home />} />
-        <Route path="/produtos/estacoes" element={<Home />} />
-        <Route path="/boletins" element={<Home />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="loader-wrapper">
+            <img src="/liao_loader.gif" alt="Carregando" className="loader-image" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/produtos/radar" />} />
+          <Route path="/sobre" element={<Home />} />
+          <Route path="/produtos/radar" element={<Home />} />
+          <Route path="/produtos/satelite" element={<Home />} />
+          <Route path="/produtos/estacoes" element={<Home />} />
+          <Route path="/boletins" element={<Home />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
